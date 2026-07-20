e54b0630e331978cdd5bfa565c1cec5d25ca8709
yesekayumpa <yesekayumpa@gmail.com>
2026-07-20 09:56:49 +0000

    Fix missing italic font in PDF document

--- src/components/PdfDocument.jsx
+++ src/components/PdfDocument.jsx
@@ -5,6 +5,7 @@ Font.register({
   family: 'Open Sans',
   fonts: [
     { src: 'https://cdn.jsdelivr.net/npm/open-sans-all@0.1.3/fonts/open-sans-regular.ttf' },
+    { src: 'https://cdn.jsdelivr.net/npm/open-sans-all@0.1.3/fonts/open-sans-italic.ttf', fontStyle: 'italic' },
     { src: 'https://cdn.jsdelivr.net/npm/open-sans-all@0.1.3/fonts/open-sans-700.ttf', fontWeight: 700 },
   ]
 });
