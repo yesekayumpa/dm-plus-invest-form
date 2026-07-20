c907e8d8093168b63f1fd85a17036e0a9c8c0633
yesekayumpa <yesekayumpa@gmail.com>
2026-07-20 12:43:32 +0000

    style: align conditions button to the left

--- src/App.jsx
+++ src/App.jsx
@@ -1426,7 +1426,7 @@ function App() {
 
                         {/* Conditions Générales */}
                         <div className="bg-[#deb833]/5 border border-[#deb833]/20 p-4 rounded-xl space-y-3">
-                          <div className="flex justify-end mb-2">
+                          <div className="flex justify-start mb-2">
                             <button
                               type="button"
                               onClick={(e) => { e.stopPropagation(); setShowConditions(true); }}
